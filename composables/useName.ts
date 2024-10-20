c8a9c88ea37f59072900fe346f865681e85c26e8
Mateja Petrovic <mateja176@gmail.com>
2024-10-20 17:01:01 +0200

    replaces computed with toRefs

--- composables/useName.ts
+++ composables/useName.ts
@@ -1,15 +1,13 @@
 import { useState } from 'nuxt/app';
-import { computed } from 'vue';
+import { toRefs } from 'vue';
 
 export const useName = (initialName = '') => {
   const nameState = useState<{ name: string }>('name', () => {
     return { name: initialName };
   });
-  const name = computed(() => {
-    return nameState.value.name;
-  });
+  const { name } = toRefs(nameState.value);
   const setName = (newName: string) => {
-    nameState.value.name = newName;
+    name.value = newName;
   };
   return [name, setName] as const;
 };
